c0e4e55c9a197c5f6921b4ca855934e7bfebaace
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2021-08-23 17:44:21 -0700

    Initial Commit

---
+++ src/nbaron_cs4450_hw1/Main.java
@@ -0,0 +1,29 @@
+/*
+ * file: Main.java
+ * author: N. Baron
+ * class: CS 4450 - Computer Graphics
+ *
+ * assignment: Program 1
+ * date last modified: 8/23/2021
+ *
+ * purpose: This program reads a coordinates.txt file and renders it in LWJGL.
+ * It uses glVertex2f to render each shape point-by-point.
+ * Pressing ESC will close the window.
+ */
+package nbaron_cs4450_hw1;
+
+import org.lwjgl.Sys;
+
+public class Main {
+
+    /**
+     * method: main
+     * purpose: run the whole program
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(Sys.getVersion());
+    }
+
+}
