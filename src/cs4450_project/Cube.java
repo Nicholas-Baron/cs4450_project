bd4fe072cc51ac9b2c748e2558a390da696df9ca
Nicholas Baron <nicholas.baron.ten@gmail.com>
2021-10-04 22:01:15 -0700

    Draw a single cube (#1)
    * Draw a single cube with 1 color
    
    * Make the cube faces have different colors

---
+++ src/cs4450_project/Cube.java
@@ -0,0 +1,77 @@
+/*
+ * file: Cube.java
+ * author: N. Baron
+ * class: CS 4450 - Computer Graphics
+ *
+ * assignment: Project
+ * date last modified: 10/2/2021
+ *
+ * purpose: This class represents 1 cube in 3d space.
+ */
+package cs4450_project;
+
+import static org.lwjgl.opengl.GL11.*;
+
+public class Cube {
+
+    private float size, x, y, z;
+
+    // method: constructor
+    // purpose: initialize the cube
+    // with the point (x, y, z) at its bottom-left corner.
+    public Cube(float x, float y, float z, float size) {
+        this.size = size;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    // method: draw
+    // purpose: draw this cube at its location
+    public void draw() {
+        glBegin(GL_QUADS);
+        { // Front
+            glColor3f(1, 1, 0);
+            glVertex3f(x, y, z);
+            glVertex3f(x + size, y, z);
+            glVertex3f(x + size, y + size, z);
+            glVertex3f(x, y + size, z);
+        }
+        { // Back
+            glColor3f(1, 0, 0);
+            glVertex3f(x, y, z + size);
+            glVertex3f(x + size, y, z + size);
+            glVertex3f(x + size, y + size, z + size);
+            glVertex3f(x, y + size, z + size);
+        }
+        { // Left
+            glColor3f(0, 1, 0);
+            glVertex3f(x, y, z);
+            glVertex3f(x, y, z + size);
+            glVertex3f(x, y + size, z + size);
+            glVertex3f(x, y + size, z);
+        }
+        { // Right
+            glColor3f(1, 0, 1);
+            glVertex3f(x + size, y, z);
+            glVertex3f(x + size, y, z + size);
+            glVertex3f(x + size, y + size, z + size);
+            glVertex3f(x + size, y + size, z);
+        }
+        { // Top
+            glColor3f(1, 1, 1);
+            glVertex3f(x, y + size, z);
+            glVertex3f(x + size, y + size, z);
+            glVertex3f(x + size, y + size, z + size);
+            glVertex3f(x, y + size, z + size);
+        }
+        { // Bottom
+            glColor3f(0, 1, 1);
+            glVertex3f(x, y, z);
+            glVertex3f(x + size, y, z);
+            glVertex3f(x + size, y, z + size);
+            glVertex3f(x, y, z + size);
+        }
+        glEnd();
+    }
+}
