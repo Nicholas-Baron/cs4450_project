94eede2aaf6587e5cc51917cd35f84374f6ac103
Nicholas Baron <nicholas.baron.ten@gmail.com>
2021-10-22 12:55:49 -0700

    Initial implementation of block (#4)

---
+++ src/cs4450_project/Block.java
@@ -0,0 +1,50 @@
+/*
+ * file: Block.java
+ * author: N. Baron
+ * class: CS 4450 - Computer Graphics
+ *
+ * assignment: Project
+ * date last modified: 10/2/2021
+ *
+ * purpose: This class represents 1 block in a chunk.
+ */
+package cs4450_project;
+
+public class Block {
+
+    private boolean isActive;
+    private final BlockType type;
+    private float x, y, z;
+
+    // method: constructor
+    // purpose: initialize the type of the block
+    public Block(BlockType type) {
+        this.type = type;
+    }
+
+    // method: getID
+    // purpose: return the ID of the block
+    public int getID() {
+        return type.getID();
+    }
+
+    // method: isActive
+    // purpose: return whether the block is active
+    public boolean isActive() {
+        return isActive;
+    }
+
+    // method: setActive
+    // purpose: set whether the block is active
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    // method: setCoords
+    // purpose: set the coordinates of the block
+    public void setCoords(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
---
+++ src/cs4450_project/BlockType.java
@@ -0,0 +1,37 @@
+/*
+ * file: BlockType.java
+ * author: N. Baron
+ * class: CS 4450 - Computer Graphics
+ *
+ * assignment: Project
+ * date last modified: 10/2/2021
+ *
+ * purpose: This class stores data for a single type of block.
+ */
+package cs4450_project;
+
+public enum BlockType {
+
+    Grass(0), Sand(1), Stone(2), Water(3), Dirt(4), Bedrock(5);
+
+    private int blockID;
+
+    // method: constructor
+    // purpose: initialize the ID of the blocktype
+    private BlockType(int id) {
+        blockID = id;
+    }
+
+    // method: getID
+    // purpose: return the ID of the blocktype
+    public int getID() {
+        return blockID;
+    }
+
+    // method: setID
+    // purpose: set the ID of the blocktype
+    public void setID(int id) {
+        blockID = id;
+    }
+
+}
